68a395490b096fefaec4e4e48867b8f6182b51e7
Markus Laurila <121898711+MarkLaur@users.noreply.github.com>
2023-06-06 08:05:03 +0300

    Implemented COM object dll. Updated packages.

--- ContextMenu/dllmain.cpp
+++ ContextMenu/dllmain.cpp
@@ -1,5 +1,27 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
+#include <wrl/module.h>
+#include <wrl/implements.h>
+#include <wrl/client.h>
+#include <shobjidl_core.h>
+#include <wil\resource.h>
+#include <string>
+#include <vector>
+#include <sstream>
+
+#pragma comment(lib, "runtimeobject.lib")
+
+using namespace Microsoft::WRL;
+
+//Key root directory
+#define HKEY_ROOT HKEY_CURRENT_USER
+//Registry key directory
+constexpr auto HKEY_DIR = L"Software\\MyCompany\\ContextEditor";
+//Key that contains the element's name
+constexpr auto NameKey = L"ElementName";
+//Default string for failed registry queries
+constexpr auto DefaultString = L"DefaultString";
+
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -17,3 +39,121 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+class MenuElementBase : public RuntimeClass<RuntimeClassFlags<ClassicCom>, IExplorerCommand, IObjectWithSite>
+{
+public:
+    virtual const wchar_t* Title() = 0;
+    virtual const EXPCMDFLAGS Flags() { return ECF_DEFAULT; }
+    virtual const EXPCMDSTATE State(_In_opt_ IShellItemArray* selection) { return ECS_ENABLED; }
+
+    // IExplorerCommand
+    IFACEMETHODIMP GetTitle(_In_opt_ IShellItemArray* items, _Outptr_result_nullonfailure_ PWSTR* name)
+    {
+        *name = nullptr;
+        auto title = wil::make_cotaskmem_string_nothrow(Title());
+        RETURN_IF_NULL_ALLOC(title);
+        *name = title.release();
+        return S_OK;
+    }
+    IFACEMETHODIMP GetIcon(_In_opt_ IShellItemArray*, _Outptr_result_nullonfailure_ PWSTR* icon) { *icon = nullptr; return E_NOTIMPL; }
+    IFACEMETHODIMP GetToolTip(_In_opt_ IShellItemArray*, _Outptr_result_nullonfailure_ PWSTR* infoTip) { *infoTip = nullptr; return E_NOTIMPL; }
+    IFACEMETHODIMP GetCanonicalName(_Out_ GUID* guidCommandName) { *guidCommandName = GUID_NULL;  return S_OK; }
+    IFACEMETHODIMP GetState(_In_opt_ IShellItemArray* selection, _In_ BOOL okToBeSlow, _Out_ EXPCMDSTATE* cmdState)
+    {
+        *cmdState = State(selection);
+        return S_OK;
+    }
+    IFACEMETHODIMP Invoke(_In_opt_ IShellItemArray* selection, _In_opt_ IBindCtx*) noexcept try
+    {
+        HWND parent = nullptr;
+        if (m_site)
+        {
+            ComPtr<IOleWindow> oleWindow;
+            RETURN_IF_FAILED(m_site.As(&oleWindow));
+            RETURN_IF_FAILED(oleWindow->GetWindow(&parent));
+        }
+
+        std::wostringstream title;
+        title << Title();
+
+        if (selection)
+        {
+            DWORD count;
+            RETURN_IF_FAILED(selection->GetCount(&count));
+            title << L" (" << count << L" selected items)";
+        }
+        else
+        {
+            title << L"(no selected items)";
+        }
+
+        MessageBox(parent, title.str().c_str(), L"TestCommand", MB_OK);
+        return S_OK;
+    }
+    CATCH_RETURN();
+
+    IFACEMETHODIMP GetFlags(_Out_ EXPCMDFLAGS* flags) { *flags = Flags(); return S_OK; }
+    IFACEMETHODIMP EnumSubCommands(_COM_Outptr_ IEnumExplorerCommand** enumCommands) { *enumCommands = nullptr; return E_NOTIMPL; }
+
+    // IObjectWithSite
+    IFACEMETHODIMP SetSite(_In_ IUnknown* site) noexcept { m_site = site; return S_OK; }
+    IFACEMETHODIMP GetSite(_In_ REFIID riid, _COM_Outptr_ void** site) noexcept { return m_site.CopyTo(riid, site); }
+
+protected:
+    ComPtr<IUnknown> m_site;
+};
+
+class __declspec(uuid("b6046922-c69e-45c7-bab9-e9f760052b5d")) MenuElementHandler final : public MenuElementBase
+{
+public:
+    const wchar_t* Title() override
+    {
+        return L"testelem";
+
+        //Open registry key
+        HKEY hKey;
+        LONG lRes = RegOpenKeyEx(HKEY_ROOT, HKEY_DIR, 0, KEY_READ, &hKey);
+        bool bExistsAndSuccess(lRes == ERROR_SUCCESS);
+        bool bDoesNotExistsSpecifically(lRes == ERROR_FILE_NOT_FOUND);
+
+        //if (!bExistsAndSuccess) return L"failure";
+        if (bDoesNotExistsSpecifically) return L"file not found";
+        //if (!bExistsAndSuccess) return std::to_wstring(lRes);
+
+        //Read value from key
+        name = DefaultString;
+
+        WCHAR szBuffer[512];
+        DWORD dwBufferSize = sizeof(szBuffer);
+        ULONG nError;
+        nError = RegQueryValueEx(hKey, NameKey, 0, NULL, (LPBYTE)szBuffer, &dwBufferSize);
+        if (ERROR_SUCCESS == nError)
+        {
+            name = szBuffer;
+        }
+
+        return name.c_str();
+    }
+    const EXPCMDSTATE State(_In_opt_ IShellItemArray* selection) override { return ECS_DISABLED; }
+
+private:
+    std::wstring name;
+};
+
+CoCreatableClass(MenuElementHandler)
+CoCreatableClassWrlCreatorMapInclude(MenuElementHandler)
+
+STDAPI DllGetActivationFactory(_In_ HSTRING activatableClassId, _COM_Outptr_ IActivationFactory** factory)
+{
+    return Module<ModuleType::InProc>::GetModule().GetActivationFactory(activatableClassId, factory);
+}
+
+STDAPI DllCanUnloadNow()
+{
+    return Module<InProc>::GetModule().GetObjectCount() == 0 ? S_OK : S_FALSE;
+}
+
+STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _COM_Outptr_ void** instance)
+{
+    return Module<InProc>::GetModule().GetClassObject(rclsid, riid, instance);
+}
